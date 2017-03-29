b3a8536b91d5ef2002693a3a0c6e37d226d3dc99
Mac Ryndziewicz <fuji.two6@gmail.com>
2017-03-29 15:33:55 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+﻿var femaleNames = ['Asia', 'Kasia', 'Ola', 'Agata'];
+var maleNames = ['Maciek', 'Grzegorz', 'Marcin', 'Michał'];
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName = 'Bogdan';
+
+var indexOfNewName = allNames.indexOf(newName);
+console.log('szukana wartość znajduje się na ' + indexOfNewName + ' pozycji tablicy');
+
+if (allNames.indexOf(newName) === -1) {
+    allNames.push(newName);
+} else {
+    alert('Takie imie jest już na tablicy');
+    console.log('Takie imie jest już na tablicy');
+}
